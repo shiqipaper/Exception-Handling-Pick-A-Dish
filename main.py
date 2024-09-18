92fed45a22691d74f9321891d7fae53c33605374
luqi <lq.xie.work@gmail.com>
2024-09-18 11:38:21 -0700

    complete

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ def select_dish(foods, selected_food):
     print(f"Ah, {foods[selected_food]}! An excellent choice!")
 
 def your_menu(foods):
+    if type(foods) != list:
+        raise TypeError("Please enter a list")
+    
     try:
         index = 1
         for dish in foods:
@@ -9,10 +12,20 @@ def your_menu(foods):
             index += 1
     
         selected_choice = int(input("Your order number? "))
-        select_dish(foods, selected_choice - 1)
+        if selected_choice < 1:
+            raise ValueError(selected_choice)
+        else:
+            select_dish(foods, selected_choice - 1)
     except IndexError as error:
         print(f"{error} was entered.")
         print("Next time try entering something on the menu!")
+    except ValueError as error:
+        print(f"{error} was entered.")
+        print("Next time try to enter a number on the menu!")
+    
+    
+    
+
 
 menu_items = [
     "Yakisoba",
